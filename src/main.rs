517853420cf379b83e872709a10ba5b73dfaae85
Viet Le <vietlq85@gmail.com>
2020-04-21 21:48:18 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+extern crate btleplug;
+extern crate rand;
+
+use btleplug::api::{Central, Peripheral, UUID};
+#[cfg(target_os = "linux")]
+use btleplug::bluez::{adapter::ConnectedAdapter, manager::Manager};
+#[cfg(target_os = "macos")]
+use btleplug::corebluetooth::{adapter::Adapter, manager::Manager};
+#[cfg(target_os = "windows")]
+use btleplug::winrtble::{adapter::Adapter, manager::Manager};
+use rand::{thread_rng, Rng};
+use std::thread;
+use std::time::Duration;
+
+// adapter retrieval works differently depending on your platform right now.
+// API needs to be aligned.
+
+#[cfg(any(target_os = "windows", target_os = "macos"))]
+fn get_central(manager: &Manager) -> Adapter {
+    let adapters = manager.adapters().unwrap();
+    adapters.into_iter().nth(0).unwrap()
+}
+
+#[cfg(target_os = "linux")]
+fn get_central(manager: &Manager) -> ConnectedAdapter {
+    let adapters = manager.adapters().unwrap();
+    let adapter = adapters.into_iter().nth(0).unwrap();
+    adapter.connect().unwrap()
+}
+
+pub fn main() {
+    let manager = Manager::new().unwrap();
+
+    // get the first bluetooth adapter
+    //
+    // connect to the adapter
+    let central = get_central(&manager);
+
+    // start scanning for devices
+    central.start_scan().unwrap();
+    // instead of waiting, you can use central.on_event to be notified of
+    // new devices
+    thread::sleep(Duration::from_secs(10));
+
+    for p in central.peripherals() {
+        match p.discover_characteristics() {
+            Ok(v) => println!("Got characteristics: {:?}", v),
+            Err(e) => println!("Got errors when querying characteristics: {:?}", e),
+        };
+        println!("p.properties = {:?}", p.properties());
+        println!("p.characteristics = {:?}", p.characteristics());
+    }
+
+    /*
+    // find the device we're interested in
+    let light = central
+        .peripherals()
+        .into_iter()
+        .find(|p| {
+            p.properties()
+                .local_name
+                .iter()
+                .any(|name| name.contains("LEDBlue"))
+        })
+        .unwrap();
+
+    // connect to the device
+    light.connect().unwrap();
+
+    // discover characteristics
+    light.discover_characteristics().unwrap();
+
+    // find the characteristic we want
+    let chars = light.characteristics();
+    let cmd_char = chars.iter().find(|c| c.uuid == UUID::B16(0xFFE9)).unwrap();
+
+    // dance party
+    let mut rng = thread_rng();
+    for _ in 0..20 {
+        let color_cmd = vec![0x56, rng.gen(), rng.gen(), rng.gen(), 0x00, 0xF0, 0xAA];
+        light.command(&cmd_char, &color_cmd).unwrap();
+        thread::sleep(Duration::from_millis(200));
+    }
+    */
+}
